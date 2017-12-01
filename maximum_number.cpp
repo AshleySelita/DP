97d83d9457661a75f1815d244e53e03357beb70a
mzq667 <x8888888888___0@163.com>
2017-12-01 21:49:24 +0800

    thrid

--- maximum_number.cpp
+++ maximum_number.cpp
@@ -3,11 +3,15 @@
 int a[1001], f[1001];
 int n;
 int main() {
+    freopen("in.in", "r", stdin);
     scanf("%d", &n);
     for (int i = 1; i <= n; ++i) {
         int tmp = i, yue = 2;
         while((tmp ^ 1)) {
-            if(!(tmp % yue)) a[i] = a[i] + yue, yue++;
+            if(!(tmp % yue)) {
+                while(!(tmp % yue)) a[i] = a[i] + yue, tmp = tmp / yue;
+                yue++;
+            }
             else yue++;
         }
         a[i]++;
@@ -15,5 +19,6 @@ int main() {
     for (int i = 1; i <= n; ++i) 
         for (int j = n; j >= i; --j)
             f[j] = std::max(f[j], f[j - i] + a[i]);
+    printf("%d\n", f[n]);
     return 0;
 }
