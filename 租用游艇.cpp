794139db28d72521f2690f5693abf031aa1f7915
mzq667 <x8888888888___0@163.com>
2017-12-01 21:12:37 +0800

    third

--- 租用游艇.cpp
+++ 租用游艇.cpp
@@ -1,19 +1,19 @@
 #include <algorithm>
 #include <cstdio>
 #include <cstring>
-int a[201][201], f[201];
+int a[201][201];
 int n;
 int main() {
-    memset(f, 127, sizeof f);
     memset(a, 127, sizeof a);
-    f[1] = 0;
     scanf("%d", &n);
+    for (int i = 1; i <= n; ++i) a[i][i] = 0;
     for (int i = 1; i <= n; ++i)
         for (int j = i + 1; j <= n; ++j)
-            scanf("%d", &a[i][j]), a[j][i] = a[i][j];
-    for (int i = 1; i <= n; ++i)
-        for (int j = 1; j < i; ++j)
-            f[i] = std::min(f[i], f[i - j] + a[j][i]);
-    printf("%d\n", f[n]);
+            scanf("%d", &a[i][j]);
+    for (int k = 1; k <= n; ++k)
+        for (int i = 1; i <= n; ++i)
+            for (int j = 1; j <= n; ++j)
+                a[i][j] = std::min(a[i][j], a[i][k] + a[k][j]);
+    printf("%d\n", a[1][n]);
     return 0;
 }
