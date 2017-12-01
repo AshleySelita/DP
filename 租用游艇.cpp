6c219589a53106c750539a516c8651637c96ee24
mzq667 <x8888888888___0@163.com>
2017-12-01 20:57:01 +0800

    second
    Signed-off-by: mzq667 <x8888888888___0@163.com>

--- 租用游艇.cpp
+++ 租用游艇.cpp
@@ -1,1 +1,19 @@
+#include <algorithm>
 #include <cstdio>
+#include <cstring>
+int a[201][201], f[201];
+int n;
+int main() {
+    memset(f, 127, sizeof f);
+    memset(a, 127, sizeof a);
+    f[1] = 0;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; ++i)
+        for (int j = i + 1; j <= n; ++j)
+            scanf("%d", &a[i][j]), a[j][i] = a[i][j];
+    for (int i = 1; i <= n; ++i)
+        for (int j = 1; j < i; ++j)
+            f[i] = std::min(f[i], f[i - j] + a[j][i]);
+    printf("%d\n", f[n]);
+    return 0;
+}
